4edf0753941f6e8eb01e7cd6ea6349df245b5dff
Gamerboi7692 <92458129+Gamerboi7692@users.noreply.github.com>
2022-09-11 23:08:43 +0530

    Add files via upload
    ver 0,1 (more broken)

--- ultimate epix racing game/cargltf.gd
+++ ultimate epix racing game/cargltf.gd
@@ -7,16 +7,26 @@ func _process(delta):
 		engine_force += 1.1
 		engine_force = clamp(engine_force, 0, 400)
 	else:
-		engine_force = 0
+		engine_force -= 1.1
+		engine_force = clamp(engine_force, 0, 400)
 	
 	if Input.is_action_pressed("right"):
-		steering = -1
+		print("no")
+		steering += -1.1
+		steering = clamp(steering, 0, 1.5)
+	elif Input.is_action_pressed("left"):
+		print("yo")
+		steering += 1.1
+		steering = clamp(steering, 0, 1.5)
 	else:
 		steering = 0
 	
-	if Input.is_action_pressed("left"):
-		steering = 1
-	else:
-		steering = 0
-	
+#	if Input.is_action_pressed("left"):
+#		print("yo")
+#		steering += 1.1
+#		steering = clamp(steering, 0, -2)
+#	else:
+#		steering -= 1.1
+#		steering = clamp(steering, 0, -2)
+#
 
