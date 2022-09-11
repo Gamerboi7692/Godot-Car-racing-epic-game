c1e69a2ae76c461ef7bfc2245116c0dd33766680
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2022-09-11 20:54:34 +0200

    Made the car steerable... kinda
    The logic was fine but in the wrong script.
    Some buttons still don't do anything

--- ultimate epix racing game/Main.gd
+++ ultimate epix racing game/Main.gd
@@ -1,2 +1,46 @@
-a
+extends Spatial
 
+""""
+The main node is where all the 'magic' happens.
+It is the true thing, that gets the input and
+passes it down to the car node.
+"""
+var car : VehicleBody #we need to store the instance of the car
+
+func _ready():
+	car = $cargltf #get the car instance by node reference
+
+#Process the input every frame
+func _process(delta):
+	if Input.is_action_pressed("ui_up"):
+		#set the car's force via dot-notation: node.property
+		car.engine_force += 1.1
+		car.engine_force = clamp(car.engine_force, 0, 400)
+	
+	#down doesn't work for some reason
+	elif Input.is_action_pressed("ui_down"):
+		car.engine_force -= 1.1
+		car.engine_force = clamp(car.engine_force, 0, 400)
+	else:
+		car.engine_force = 0
+	
+	#right doesn't work for some reason
+	if Input.is_action_pressed("ui_right"):
+		print("no")
+		car.steering += -1.1
+		car.steering = clamp(car.steering, 0, 1.5)
+	
+	elif Input.is_action_pressed("ui_left"):
+		print("yo")
+		car.steering += 1.1
+		car.steering = clamp(car.steering, 0, 1.5)
+	else:
+		car.steering = 0
+	
+#	if Input.is_action_pressed("left"):
+#		print("yo")
+#		steering += 1.1
+#		steering = clamp(steering, 0, -2)
+#	else:
+#		steering -= 1.1
+#		steering = clamp(steering, 0, -2)
--- ultimate epix racing game/cargltf.gd
+++ ultimate epix racing game/cargltf.gd
@@ -2,24 +2,24 @@ extends VehicleBody
 
 
 func _process(delta):
+	pass
+	#if Input.is_action_pressed("up"):
+	#	engine_force += 1.1
+	#	engine_force = clamp(engine_force, 0, 400)
+	#else:
+	#	engine_force -= 1.1
+	#	engine_force = clamp(engine_force, 0, 400)
 	
-	if Input.is_action_pressed("up"):
-		engine_force += 1.1
-		engine_force = clamp(engine_force, 0, 400)
-	else:
-		engine_force -= 1.1
-		engine_force = clamp(engine_force, 0, 400)
-	
-	if Input.is_action_pressed("right"):
-		print("no")
-		steering += -1.1
-		steering = clamp(steering, 0, 1.5)
-	elif Input.is_action_pressed("left"):
-		print("yo")
-		steering += 1.1
-		steering = clamp(steering, 0, 1.5)
-	else:
-		steering = 0
+	#if Input.is_action_pressed("right"):
+	#	print("no")
+	#	steering += -1.1
+	#	steering = clamp(steering, 0, 1.5)
+	#elif Input.is_action_pressed("left"):
+	#	print("yo")
+	#	steering += 1.1
+	#	steering = clamp(steering, 0, 1.5)
+	#else:
+	#	steering = 0
 	
 #	if Input.is_action_pressed("left"):
 #		print("yo")
